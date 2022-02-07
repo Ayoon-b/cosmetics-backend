3e26d643bed2618bbf91edbd1f1857339f7bd337
Ayoon-b <wnr0303@gmail.com>
2022-02-07 23:40:25 +0900

    feat: JsonObject로부터 Product 생성하는 기능 추가
    및 테스트 추가

--- src/main/java/com/teamtbd/cosmetics/product/service/ProductService.java
+++ src/main/java/com/teamtbd/cosmetics/product/service/ProductService.java
@@ -13,4 +13,24 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    public Product getProductFromJsonObject(JSONObject jsonObject) {
+        return Product.builder()
+                .id(String.valueOf(jsonObject.get("id")))
+                .name(String.valueOf(jsonObject.get("name")))
+                .price(Integer.parseInt(String.valueOf(jsonObject.get("price"))))
+                .brand(String.valueOf(jsonObject.get("brand")))
+                .imageUrl(String.valueOf(jsonObject.get("image_url")))
+                .category(Category.valueOf(Integer.parseInt(String.valueOf(jsonObject.get("category")))))
+                .site(Site.valueOf(Integer.parseInt(String.valueOf(jsonObject.get("site")))))
+                .siteProductId(String.valueOf(jsonObject.get("site_id")))
+                .link(String.valueOf(jsonObject.get("link")))
+                .siteCategoryId(String.valueOf(jsonObject.get("site_category")))
+                .volume(String.valueOf(jsonObject.get("volume")))
+                .expirationDate(String.valueOf(jsonObject.get("expiration_date")))
+                .prodUsage(String.valueOf(jsonObject.get("usage")))
+                .origin(String.valueOf(jsonObject.get("origin")))
+                .ingredients(String.valueOf(jsonObject.get("ingredients")))
+                .caution(String.valueOf(jsonObject.get("caution")))
+                .build();
+    }
 }
