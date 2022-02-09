6f05d5ca2adc279508e1f721854ed04308d3e5ab
Ayoon-b <wnr0303@gmail.com>
2022-02-09 22:07:24 +0900

    feat: product 컬럼 수정

--- src/main/java/com/teamtbd/cosmetics/product/Product.java
+++ src/main/java/com/teamtbd/cosmetics/product/Product.java
@@ -42,6 +42,7 @@ public class Product {
     private Category category;
 
     @Column
+    @Lob
     private String ingredients;
 
     @Column
@@ -57,22 +58,24 @@ public class Product {
     @Column
     private String siteCategoryId;
 
-    @Column
+    @Column(length = 512)
     private String link;
 
     @Column
     private String volume;
 
     @Column
+    @Lob
     private String prodUsage;
 
     @Column
+    @Lob
     private String caution;
 
     @Column
     private String expirationDate;
 
-    @Column
+    @Column(length = 512)
     private String imageUrl;
 
     @PrePersist
