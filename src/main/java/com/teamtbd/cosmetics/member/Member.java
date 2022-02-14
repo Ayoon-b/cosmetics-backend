8084e6333fb562eb6970fe5a5f99a60ba487d284
hyobinkim <hyobim0911@gmail.com>
2022-02-14 16:06:03 +0900

    chore : Setter 어노테이션 추가

--- src/main/java/com/teamtbd/cosmetics/member/Member.java
+++ src/main/java/com/teamtbd/cosmetics/member/Member.java
@@ -1,33 +1,46 @@
-package com.teamtbd.cosmetics.domain;
+package com.teamtbd.cosmetics.member;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
 
-@Entity
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity //JPA
+@Builder // JPA 엔티티 객체생성자 빌더
+@NoArgsConstructor //파라미터가 없는 기본 생성자 생성
+@AllArgsConstructor //모든 필드 값 받는 파라미터 생성자 생성
+@ToString
+@Getter //접근자 자동생성    // @Data 어노테이션을 쓰면 한번에 여러 어노테이션 처리가능
+@Setter
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //디비가 아이디 자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //일단 디비가 아이디 자동 생성되게
+    //소셜 로그인 하면 어떤식으로 들어오는지 모름 아직 key를 안정함
     private Long id;
-    private String name;
 
-    public String getName() {
-        return name;
-    }
+    @Column
+    private String user_name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Column
+    private String nickname;
 
-    public Long getId() {
-        return id;
-    }
+    @Column
+    private String email;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column
+    private String phonenumber;
 
+    @Column
+    private String gender;
+
+    @Column
+    private String birth;
+
+    @Column
+    private String skin_type;
+
+    @Column
+    private String join_date;
 
 }
