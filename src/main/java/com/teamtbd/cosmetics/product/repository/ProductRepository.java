fb74fa24c0ca90d562021db6749fcb181c0899ec
Ayoon-b <wnr0303@gmail.com>
2022-02-14 15:51:21 +0900

    feat: findBySite 기능 추가

--- src/main/java/com/teamtbd/cosmetics/product/repository/ProductRepository.java
+++ src/main/java/com/teamtbd/cosmetics/product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.teamtbd.cosmetics.product.repository;
 
 import com.teamtbd.cosmetics.domain.Category;
+import com.teamtbd.cosmetics.domain.Site;
 import com.teamtbd.cosmetics.product.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,5 +17,7 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findByNameContains(String name);
     List<Product> findByNameContains(String name, Pageable pageable);
 
+    List<Product> findBySite(Site site);
+    List<Product> findBySite(Site site, Pageable pageable);
 
 }
