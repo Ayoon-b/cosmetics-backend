82498822eb849e4281f850f7b71481d593d187f6
hyobinkim <hyobim0911@gmail.com>
2022-02-15 13:52:32 +0900

    chore : Service , repository 어노테이션 등록

--- src/main/java/com/teamtbd/cosmetics/member/repository/MemberRepository.java
+++ src/main/java/com/teamtbd/cosmetics/member/repository/MemberRepository.java
@@ -2,12 +2,10 @@ package com.teamtbd.cosmetics.member.repository;
 
 import com.teamtbd.cosmetics.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
-public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>, MemberRepository {
-
-    @Override
-    Optional<Member> findByName(String name);
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long>{
 
 }
