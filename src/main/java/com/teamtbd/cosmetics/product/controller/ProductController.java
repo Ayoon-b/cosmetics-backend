d01d2113f2e17c4fdc03fdcc75271d7441373eeb
Ayoon-b <wnr0303@gmail.com>
2022-02-16 21:59:16 +0900

    fix: getProducts 기능 수정

--- src/main/java/com/teamtbd/cosmetics/product/controller/ProductController.java
+++ src/main/java/com/teamtbd/cosmetics/product/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.teamtbd.cosmetics.product.controller;
 
+import com.teamtbd.cosmetics.domain.Category;
 import com.teamtbd.cosmetics.product.Product;
 import com.teamtbd.cosmetics.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,26 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping(path = "/products")
-    public Page<Product> getProducts(@RequestParam(defaultValue = "0") Integer page){
-        return productService.getProducts(PageRequest.of(page, 24));
+    public Page<Product> getProducts(@RequestParam(defaultValue = "0") Integer page,
+                                     @RequestParam(value = "category_id", required = false) Integer categoryId,
+                                     @RequestParam(value = "search", required = false) String search,
+                                     @RequestParam(value = "sortBy", defaultValue = "price") String sortBy,
+                                     @RequestParam(value = "orderBy", defaultValue = "asc") String orderBy){
+        Sort sort = Sort.by(orderBy.equalsIgnoreCase("desc")
+                ? Sort.Order.desc(sortBy)
+                : Sort.Order.asc(sortBy));
+        PageRequest pageable = PageRequest.of(page, 24, sort);
+
+        if (categoryId != null) {
+            Category category = Category.valueOf(categoryId);
+            if (search != null) {
+                return productService.getProductsByCategoryAndName(category, search, pageable);
+            }
+            return productService.getProductsByCategory(category, pageable);
+        }
+        if (search != null) {
+            return productService.getProductsByNameContains(search, pageable);
+        }
+        return productService.getProducts(pageable);
     }
 }
