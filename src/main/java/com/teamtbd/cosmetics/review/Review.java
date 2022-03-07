7e46f4967d4d5d1de38a0981b04676a8a6c82350
YeoMinYoung <dufmasky99@naver.com>
2022-03-07 20:40:31 +0900

    fix: review service 매소드 이름 수정

--- src/main/java/com/teamtbd/cosmetics/review/Review.java
+++ src/main/java/com/teamtbd/cosmetics/review/Review.java
@@ -13,6 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @ToString
 @Getter
+@Setter
 //m:n연결 필요 member와도 연결돼야하고, product랑도 연결돼야함
 //상품명(또는 상품의 고유 id product랑 연결), 쓴사람id(member랑 연결) , 별점, 리뷰제목, 리뷰내용, 사진
 public class Review {
@@ -20,27 +21,22 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //리뷰만의 고유id
 
-    @Column
-    private Long reviewerId;
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
 
     @ManyToOne
     @JoinColumn(name = "productId")
     private Product product;
 
-    @ManyToOne
-    @JoinColumn(name = "memberId")
-    private Member member;
-
     @Column
     private String title;
 
     @Column
-    private String review;
+    private String body;
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    private Rating rating;
-
+    @Column //예외처리를 service나 controller에서 해주기
+    private Integer rating;
     //이미지 어캐넣을건지 고민
 
 }
--- src/main/java/com/teamtbd/cosmetics/review/service/ReviewService.java
+++ src/main/java/com/teamtbd/cosmetics/review/service/ReviewService.java
@@ -1,18 +1,46 @@
 package com.teamtbd.cosmetics.review.service;
 
+import com.teamtbd.cosmetics.member.Member;
+import com.teamtbd.cosmetics.review.Review;
+import com.teamtbd.cosmetics.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional //데이터베이스의 상태를 변경, 한번에 수행되어야하는 연산
 @Service
 public class ReviewService {
     //<요구사항>
-    //리뷰작성 , 저장 -> 구매한 상품에 대해서만 작성해야하는데 이부분은 어떻게 해야할지 생각
+    //리뷰작성(프엔에서) , 저장 -> 구매한 상품에 대해서만 작성해야하는데 이부분은 어떻게 해야할지 생각
     //리뷰수정
     //리뷰삭제
     //상품별 별점 계산
     //내가 작성한 후기 목록 보기
 
+    private ReviewRepository reviewRepository;
+
+    //리뷰 저장
+    public Review save(Review review){
+        return reviewRepository.save(review);
+    }
+
+    //리뷰 수정 // 더 나은 방법 찾기
+    public Optional<Review> update(Long id, Review review){
+        Optional<Review> updateReview = reviewRepository.findById(id);
+        //바꿀수없는것 id,join date
+        updateReview.ifPresent(selectReview->{
+            if(review.getTitle()!=null)
+                selectReview.setTitle(review.getTitle());
+            if(review.getBody()!=null)
+                selectReview.setBody(review.getBody());
+            if(review.getRating()!=null)
+                selectReview.setRating(review.getRating());
+
+            reviewRepository.save(selectReview);
+        });
+        return updateReview;
+    }
 }
