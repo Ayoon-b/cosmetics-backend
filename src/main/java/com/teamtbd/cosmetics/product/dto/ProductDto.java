16963e4f085f0a21a51931bc253151091498e0d1
Ayoon-b <wnr0303@gmail.com>
2022-03-23 21:31:41 +0900

    feat: ProductDto 추가, 브랜드, 상품명, 가격, 상품이미지만 받아오게 수정

---
+++ src/main/java/com/teamtbd/cosmetics/product/dto/ProductDto.java
@@ -0,0 +1,17 @@
+package com.teamtbd.cosmetics.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class ProductDto {
+    private String name;
+    private int price;
+    private String brand;
+    private String imageUrl;
+}
